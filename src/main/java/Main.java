e5c0272b89f49b972cbd967cf58670b4d0870296
coder-5 <vihaan.agrawalcoder@gmail.com>
2025-11-26 16:05:28 -0500

    exit + linting fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,60 +1,58 @@
 import java.util.Scanner;
 
-public class Main {
+void main() {
 
-    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int input;
 
-        Scanner scanner = new Scanner(System.in);
-        boolean keepGoing = true;
-        int input = 0;
+    main_loop:
+    while (true) {
+        IO.println("0. exit");
+        IO.println("\n1. Add a task");
+        IO.println("2. Remove a task");
+        IO.println("3. Show all current tasks");
+        IO.println("4. Mark a task done");
+        IO.println("5. Edit an existing task");
+        IO.println("6. Change the priority of an existing task");
+        IO.println("7. Mark a task incomplete");
+        IO.print("\nPlease pick an action (enter a number 0 - 7): ");
 
-
-        while (keepGoing) {
-            System.out.println("\n1. Add a task");
-            System.out.println("2. Remove a task");
-            System.out.println("3. Show all current tasks");
-            System.out.println("4. Mark a task done");
-            System.out.println("5. Edit an existing task");
-            System.out.println("6. Change the priority of an existing task");
-            System.out.println("7. Mark a task incomplete");
-            System.out.print("\nPlease pick an action (enter a number 1 - 7): ");
-
-            if (scanner.hasNextInt()) {
-                input = scanner.nextInt();
-                scanner.nextLine();
-            } else {
-                System.out.println("\nThat is an invalid input.");
-                scanner.nextLine();
-                continue;
-            }
+        if (scanner.hasNextInt()) {
+            input = scanner.nextInt();
+            scanner.nextLine();
+        } else {
+            IO.println("\nThat is an invalid input.");
+            scanner.nextLine();
+            continue;
+        }
 
 
-            switch (input) {
-                case 1:
-                    TaskManager.addTask(scanner);
-                    break;
-                case 2:
-                    TaskManager.removeTask(scanner);
-                    break;
-                case 3:
-                    TaskManager.showTasks();
-                    break;
-                case 4:
-                    TaskManager.markDone(scanner);
-                    break;
-                case 5:
-                    TaskManager.editTask(scanner);
-                    break;
-                case 6:
-                    TaskManager.changePriority(scanner);
-                    break;
-                case 7:
-                    TaskManager.markTaskIncomplete(scanner);
-                    break;
-                default:
-                    System.out.println("That is an invalid input.");
-            }
+        switch (input) {
+            case 0:
+                break main_loop;
+            case 1:
+                TaskManager.addTask(scanner);
+                break;
+            case 2:
+                TaskManager.removeTask(scanner);
+                break;
+            case 3:
+                TaskManager.showTasks();
+                break;
+            case 4:
+                TaskManager.markDone(scanner);
+                break;
+            case 5:
+                TaskManager.editTask(scanner);
+                break;
+            case 6:
+                TaskManager.changePriority(scanner);
+                break;
+            case 7:
+                TaskManager.markTaskIncomplete(scanner);
+                break;
+            default:
+                IO.println("That is an invalid input.");
         }
     }
-
 }
