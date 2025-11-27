108e2537aafde2cde063577a37c2337224647cdb
coder-5 <vihaan.agrawalcoder@gmail.com>
2025-11-26 19:04:50 -0500

    new numbering system + change main menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,15 +10,13 @@ void main() {
         System.out.println("\n0. exit");
         System.out.println("1. Add a task");
         System.out.println("2. Remove a task");
-        System.out.println("3. Show all current tasks");
+        System.out.println("3. Show tasks");
         System.out.println("4. Mark a task done");
         System.out.println("5. Edit an existing task");
         System.out.println("6. Change the priority of an existing task");
         System.out.println("7. Mark a task incomplete");
-        System.out.println("8. Show only incomplete tasks");
-        System.out.println("9. Show only complete tasks");
-        System.out.println("10. Show only complete tasks");
-        System.out.print("\nPlease pick an action (enter a number 0 - 10): ");
+        System.out.println("8. Delete all complete tasks");
+        System.out.print("\nPlease pick an action (enter a number 0 - 8): ");
 
         if (scanner.hasNextInt()) {
             choice = scanner.nextInt();
@@ -41,8 +39,28 @@ void main() {
                 TaskManager.removeTask(scanner);
                 break;
             case 3:
-                TaskManager.showTasks();
-                break;
+                System.out.println("0. return to main menu");
+                System.out.println("1. Show all tasks");
+                System.out.println("2. Show incomplete tasks");
+                System.out.println("3. Show complete tasks");
+                System.out.print("\nPlease pick an action (enter a number 0 - 3): ");
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 0:
+                        break;
+                    case 1:
+                        TaskManager.showTasks();
+                        break;
+                    case 2:
+                        TaskManager.showIncompleteTasks();
+                        break;
+                    case 3:
+                        TaskManager.showCompleteTasks();
+                        break;
+                    default:
+                        System.out.println("That is an invalid input.");
+                        continue;
+                }
             case 4:
                 TaskManager.markDone(scanner);
                 break;
@@ -56,16 +74,11 @@ void main() {
                 TaskManager.markTaskIncomplete(scanner);
                 break;
             case 8:
-                TaskManager.showIncompleteTasks();
-                break;
-            case 9:
-                TaskManager.showCompleteTasks();
-                break;
-            case 10:
                 TaskManager.deleteAllCompletedTasks();
                 break;
             default:
                 System.out.println("That is an invalid input.");
+                break;
         }
     }
 }
